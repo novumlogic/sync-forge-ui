b02581a6a81a0aed89cb7bce2ec65557b915c1dd
Mihir Paldhikar <mihir.paldhikar@novumlogic.com>
2025-09-08 16:59:07 +0530

    fix: Fix missing DnDProvider in builder_layout.tsx

--- src/layouts/builder_layout.tsx
+++ src/layouts/builder_layout.tsx
@@ -1,11 +1,14 @@
+import { DnDProvider } from "@providers/dnd_provider";
 import { ReactFlowProvider } from "@xyflow/react";
 import type { JSX } from "react";
 import { Outlet } from "react-router";
 
 export default function BuilderLayout(): JSX.Element {
   return (
-    <ReactFlowProvider>
-      <Outlet />
-    </ReactFlowProvider>
+    <DnDProvider>
+      <ReactFlowProvider>
+        <Outlet />
+      </ReactFlowProvider>
+    </DnDProvider>
   );
 }
